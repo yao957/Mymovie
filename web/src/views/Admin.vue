25a987c45e6d9c00acfbdb73e4eff3e16734845b
movie <438602483@qq.com>
2021-04-26 17:03:40 +0800

    静态页面架构完成

---
+++ web/src/views/Admin.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="main">
+    <Top title="电影院"/>
+     <Login/>
+      <TabBar/>
+  </div>
+</template>
+
+<script>
+import Top from '@/components/Top';
+import TabBar from '@/components/TabBar';
+import Login from '@/components/Login';
+export default {
+  name: 'admin',
+  components:{
+      Top,
+      TabBar,
+      Login
+  },
+  data() { 
+    return {
+
+    }
+  }
+ }
+</script>
